a065a19b86cf7bf63f72bf569ea739db405e5e23
Jeremiah Clothier <jclothie@uoregon.edu>
2018-11-10 09:38:51 -0800

    first working version

--- content.js
+++ content.js
@@ -1,23 +1,50 @@
-function create_table_header(cell, title) {
+function create_table_header(cell, header_title) {
+  console.log("Creating Table Header")
+  console.log(cell)
   var td = document.createElement('td');
   td.setAttribute("class","dddead")
-  td.setAttribute("width", 100)
-  td.appendChild(document.createTextNode(title));
+  td.appendChild(document.createTextNode(header_title));
   cell.append(td)
 }
 
-function add_info(number, prof){
-    var tbl = document.getElementsByClassName("datadisplaytable")[0]; // table reference
-    var i; // row index
-    
-    for (i = 0; i < tbl.rows.length; i++) {
-        var row = tbl.rows[i].getElementsByClassName("dddefault");
-        console.log(row)
-    }
-                    
+function create_table_cell(cell, text){
+  // Create Table Element
+  var td = document.createElement('td');
+  td.setAttribute("class","dddefault");
+  td.appendChild(document.createTextNode(text))
+
+  // COLOR FOR CSS Here
+	if (text != "N/A") {
+		var value = parseFloat(text);
+		if (value < 2.5) {
+			td.setAttribute("id","poor_quality");
+		} else if (value < 3.5) {
+			td.setAttribute("id","average");
+		} else {
+			td.setAttribute("id","good_quality");
+		}
+	} else {
+		td.setAttribute("id","nah"); // TODO: N/A style?
+	}
+  cell.append(td)
 }
 
-function add_headers() {
+function add_table_cells(number, prof){
+    var tbl = document.getElementsByClassName("datadisplaytable")[0]; // table reference
+    var i; // row index
+
+    for (i = 0; i < tbl.rows.length; i++) {
+        var row = tbl.rows[i].getElementsByClassName("dddefault");
+        // If the length is greater than 0, we're looking at an actual table row we can append to
+        if (row.length > 0) {
+          create_table_cell(tbl.rows[i],"1.0")
+          create_table_cell(tbl.rows[i],"2.0")
+          create_table_cell(tbl.rows[i],"3.0")
+        }
+    }
+}
+
+function add_table_headers() {
   var tbl = document.getElementsByClassName("datadisplaytable")[0]; // table reference
   var i; // row index
 
@@ -41,4 +68,5 @@ function add_headers() {
   }
 }
 
-add_headers()
+add_table_headers()
+add_table_cells()
