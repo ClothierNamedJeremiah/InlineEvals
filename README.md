e7029f05f6a06db988895a6d4e82a8a1e5fa52f3
Jeremiah Clothier <jclothie@uoregon.edu>
2018-11-09 22:27:25 -0800

    updated

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # InlineEvals
+
+HEADERS
+============
+Professor, Subject, Course, Key, Clarity, Amount Learned, Instructor's Quality, Course Quality
+============
+EXAMPLE ENTRY
+Michael Hennessey, CIS, 110, FirstLastCourse, Question 6, Question 7, Question 2, Question 1
--- content.js
+++ content.js
@@ -1,1 +1,24 @@
-console.log("hello:")
+function create_table_header(cell, title) {
+  var td = document.createElement('td');
+  td.setAttribute("class","dddhead")
+  td.appendChild(document.createTextNode(title));
+  cell.append(td)
+}
+
+function add_column(title) {
+  var tbl = document.getElementsByClassName("datadisplaytable")[0]; // table reference
+  var i; // row index
+
+  for (i = 0; i < tbl.rows.length; i++) {
+    var isHeader = tbl.rows[i].getElementsByClassName("dddead")
+    // console.log(isHeader);
+    if (isHeader) {
+      create_table_header(tbl.rows[i+2],title)
+      break;
+      i += 3;
+    }
+
+  }
+}
+
+add_column("TEST 123")
